636841e14384f9fe66d04c5d7690c7335a732271
Vincent Schinz <v.schinz@web.de>
2016-06-02 12:23:00 +0200

    Initial Commit

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using 
+        static void Main()
+        {
+            ///xxx
+            Application.EnableVisualStyles()
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThre;System.Linq;
+using System.(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    static class Program
+    {ad]
+            Application.SetCompatibleTextRenderingDefault
